423852a6c425d1e46ca076973b05da9a4b19a6b3
andres-martin <andresmartin266@gmail.com>
2019-09-25 21:51:42 -0500

    [feature] create serach component working tests passed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,36 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { keyword: '', };
+  }
+  // function to filter posts that includes given keyword
+  filterPosts = (post, search, index) => {
+    if (post.title.toLowerCase().includes(search.toLowerCase())) {
+      return (
+        <li key={index}>
+          <a href={post.url} target='blank'><img alt={post.title} src={post.image} /></a>
+          <p>{post.title}</p>
+        </li>
+      )
+    }
+  }
+  // handles change of event targeted value and set new state
+  handleChange = (e) => {
+    this.setState({ keyword: e.target.value })
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input value={this.state.keyword} onChange={this.handleChange}
+            type="text" placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {posts.map((post, index) => this.filterPosts(post, this.state.keyword, index))}
         </ul>
       </div>
     )
@@ -24,5 +43,3 @@ class App extends Component {
 
 
 export default App
-
-
